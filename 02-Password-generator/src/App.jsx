21da46c806a8585639079ca07b595b13187646f1
vidya <hansdavsh@gmail.com>
2024-04-01 01:46:43 +0530

    update the UI of the password generator project

--- 02-Password-generator/src/App.jsx
+++ 02-Password-generator/src/App.jsx
@@ -36,26 +36,26 @@ function App() {
 
   return (
     <>
-      <div className='text-center text-3xl m-5 font-semibold'>
-        <h1>Password Generator</h1>
+      <div className='text-center text-3xl m-5 p-4 font-semibold text-slate-200 bg-indigo-950 max-w-2xl mx-auto rounded'>
+        <h1>Dumb Password Generator</h1>
       </div>
-      <div className='w-full max-w-xl mx-auto shadow-md text-center bg-slate-500 p-5'>
-        <div>
-          <input ref={passwordRef} className='text-orange-500 text-3xl p-2 rounded' type="text" placeholder='Generating..' value={password} readOnly/>
-          <button onClick={copyPasswordToClipboard} className='text-3xl bg-blue-700 p-2 ml-1 rounded outline-none'>Copy</button>
+      <div className='w-full max-w-2xl mx-auto shadow-md text-center bg-slate-500 p-5 rounded'>
+        <div className='flex justify-center flex-wrap'>
+          <input ref={passwordRef} className='text-orange-500 text-xl p-2 rounded outline-none w-3/4' type="text" placeholder='Generating..' value={password} readOnly/>
+          <button onClick={copyPasswordToClipboard} className='text-2xl text-white bg-blue-700 hover:bg-blue-500 p-2 ml-1 rounded outline-none font-medium'>Copy</button>
         </div>
 
-        <div className=' flex mt-2 justify-center p-4 gap-3 text-xl'>
+        <div className='flex flex-wrap mt-2 justify-center p-4 gap-3 text-xl '>
           <div>
-            <input type="range" min={6} max={100} value={length} onChange={(e) => setLength(e.target.value)} />
-            <label >Length {length}</label>
+            <input className='mr-2' type="range" min={6} max={100} value={length} onChange={(e) => setLength(e.target.value)} />
+            <label className='text-xl  px-4 py-2' >Length {length}</label>
           </div>
           <div>
-            <input type="checkbox"  id="numberAllowed" defaultChecked={numAllowed} onChange={()=> setNumAllowed((prev) => !prev)}/>
-            <label htmlFor="numberAllowed">Numbers</label>
+            <input className='mr-2' type="checkbox"  id="numberAllowed" defaultChecked={numAllowed} onChange={()=> setNumAllowed((prev) => !prev)}/>
+            <label  htmlFor="numberAllowed">Numbers</label>
           </div>
           <div>
-            <input type="checkbox" id='charAllowed' defaultChecked={charAllowed} onChange={() => setCharAllowed((prev) => !prev)}/>
+            <input className='mr-2' type="checkbox" id='charAllowed' defaultChecked={charAllowed} onChange={() => setCharAllowed((prev) => !prev)}/>
             <label htmlFor="charAllowed">Special Character</label>
           </div>
         </div>
