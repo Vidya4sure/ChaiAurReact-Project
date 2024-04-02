ef517876ab0da45cb1eb9d873c98c6fd6861462d
vidya <hansdavsh@gmail.com>
2024-04-02 23:59:53 +0530

    improve checkbox

--- 02-Password-generator/src/App.jsx
+++ 02-Password-generator/src/App.jsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
+import Background from "./assets/Background.png"
 import './App.css'
 
 function App() {
@@ -36,27 +37,29 @@ function App() {
 
   return (
     <>
-      <div className='text-center text-3xl m-5 p-4 font-semibold text-slate-200 bg-indigo-950 max-w-2xl mx-auto rounded'>
-        <h1>Dumb Password Generator</h1>
-      </div>
-      <div className='w-full max-w-2xl mx-auto shadow-md text-center bg-slate-500 p-5 rounded'>
-        <div className='flex justify-center flex-wrap'>
-          <input ref={passwordRef} className='text-orange-500 text-xl p-2 rounded outline-none w-3/4' type="text" placeholder='Generating..' value={password} readOnly/>
-          <button onClick={copyPasswordToClipboard} className='text-2xl text-white bg-blue-700 hover:bg-blue-500 p-2 ml-1 rounded outline-none font-medium'>Copy</button>
+      <div>
+        <div className='text-center text-3xl p-4 font-semibold text-slate-200 bg-indigo-950 max-w-2xl mx-auto rounded'>
+          <h1>Dumb Password Generator</h1>
         </div>
+        <div className='w-full max-w-3xl mx-auto shadow-md text-center bg-slate-500 p-6 rounded'>
+          <div className='flex justify-center flex-wrap'>
+            <input ref={passwordRef} className='text-orange-500 text-xl p-2 rounded outline-none w-3/4' type="text" placeholder='Generating..' value={password} readOnly/>
+            <button onClick={copyPasswordToClipboard} className='text-2xl text-white bg-blue-700 hover:bg-blue-500 p-2 ml-1 rounded outline-none font-medium'>Copy</button>
+          </div>
 
-        <div className='flex flex-wrap mt-2 justify-center p-4 gap-3 text-xl '>
-          <div>
-            <input className='mr-2' type="range" min={6} max={100} value={length} onChange={(e) => setLength(e.target.value)} />
-            <label className='text-xl  px-4 py-2' >Length {length}</label>
-          </div>
-          <div>
-            <input className='mr-2' type="checkbox"  id="numberAllowed" defaultChecked={numAllowed} onChange={()=> setNumAllowed((prev) => !prev)}/>
-            <label  htmlFor="numberAllowed">Numbers</label>
-          </div>
-          <div>
-            <input className='mr-2' type="checkbox" id='charAllowed' defaultChecked={charAllowed} onChange={() => setCharAllowed((prev) => !prev)}/>
-            <label htmlFor="charAllowed">Special Character</label>
+          <div className='flex flex-wrap mt-2 justify-center p-4 gap-3 text-xl '>
+            <div>
+              <input className='mr-2' type="range" min={6} max={100} value={length} onChange={(e) => setLength(e.target.value)} />
+              <label className='text-2xl  px-4 py-2' >Length {length}</label>
+            </div>
+            <div>
+              <input className='mr-2 h-5 w-5' type="checkbox"  id="numberAllowed" defaultChecked={numAllowed} onChange={()=> setNumAllowed((prev) => !prev)}/>
+              <label className='text-2xl' htmlFor="numberAllowed">Numbers</label>
+            </div>
+            <div>
+              <input className='mr-2 h-5 w-5' type="checkbox" id='charAllowed' defaultChecked={charAllowed} onChange={() => setCharAllowed((prev) => !prev)}/>
+              <label className='text-2xl' htmlFor="charAllowed">Special Character</label>
+            </div>
           </div>
         </div>
       </div>
